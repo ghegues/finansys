e24f9b4f93b4506c8bed8dddb3a0e2de6fdde1b9
ghegues <gbrlheguedusch@hotmail.com>
2022-01-15 22:59:40 -0300

    generate components navbar  && server-error-messages

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,10 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
@@ -21,7 +24,8 @@ import { RouterModule } from '@angular/router';
   exports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NavbarComponent
   ]
 })
 export class CoreModule { }
